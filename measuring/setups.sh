dad1398f13b181a0c4763c6802da2fd4e5c611a5
Thom Wiggers <thom@thomwiggers.nl>
2020-04-23 11:57:43 +0200

    Fix kyber name in setup script

--- measuring/setups.sh
+++ measuring/setups.sh
@@ -7,6 +7,6 @@ cd ..
 # Min: cached
 ./create-experimental-setup.sh RainbowIaCyclic RainbowIaCyclic SikeP434Compressed
 # Ass: MLWE
-./create-experimental-setup.sh Dilithium2 Dilithium2 Kyber512
+./create-experimental-setup.sh Dilithium2 Dilithium2 kyber512
 # Ass: NTRU
 ./create-experimental-setup.sh Falcon512 Falcon512 ntruhps2048509
