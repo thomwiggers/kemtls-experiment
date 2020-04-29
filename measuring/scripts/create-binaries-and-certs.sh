c401a756aa7d5d92301dfc1a6664fcd0058dcad8
Thom Wiggers <thom@thomwiggers.nl>
2020-04-29 15:04:19 +0200

    Set up new experiments

--- measuring/scripts/create-binaries-and-certs.sh
+++ measuring/scripts/create-binaries-and-certs.sh
@@ -5,8 +5,10 @@ cd $(dirname $0)
 # Signed KEX
 # Classic TLS
 ./create-experimental-setup.sh X25519 RSA2048 RSA2048 RSA2048
-# Min w/o cache
+# Min w/o cache (Rainbow)
 ./create-experimental-setup.sh sikep434compressed Falcon512 XMSS RainbowIaCyclic
+# Min w/o cache (GeMSS)
+./create-experimental-setup.sh sikep434compressed Falcon512 XMSS Gemss128
 # Min w/ cache (covered below)
 #./create-experimental-setup.sh sikep434compressed Falcon512 RainbowIaCyclic RainbowIaCyclic
 # Ass: MLWE (covered below)
@@ -19,6 +21,8 @@ cd $(dirname $0)
 #./create-experimental-setup.sh sikep424compressed <ANYALG> XMSSs RainbowIaCyclic
 # Min: w/ cached + (sig falcon+cached rainbow)
 ./create-experimental-setup.sh sikep434compressed Falcon512 RainbowIaCyclic RainbowIaCyclic
+# Min: w/ cached + (sig falcon+cached gemss)
+./create-experimental-setup.sh sikep434compressed Falcon512 Gemss128 Gemss128
 # Ass: MLWE + (sig dilithium)
 ./create-experimental-setup.sh kyber512 Dilithium2 Dilithium2 Dilithium2
 # Ass: NTRU (+ sig falcon chain)
--- measuring/scripts/experiment.py
+++ measuring/scripts/experiment.py
@@ -18,18 +18,23 @@ ALGORITHMS = (
     # EXPERIMENT - KEX - LEAF - INT - ROOT
     ('sign', 'X25519', 'RSA2048', 'RSA2048', 'RSA2048'),
     ('sign', 'sikep434compressed', 'Falcon512', 'XMSS', 'RainbowIaCyclic'),
+    ('sign', 'sikep434compressed', 'Falcon512', 'XMSS', 'Gemss128'),
     ('sign', 'sikep434compressed', 'Falcon512', 'RainbowIaCyclic', 'RainbowIaCyclic'),
+    ('sign', 'sikep434compressed', 'Falcon512', 'RainbowIaCyclic', 'Gemss128'),
     ('sign', 'kyber512', 'Dilithium2', 'Dilithium2', 'Dilithium2',),
     ('sign', 'ntruhps2048509', 'Falcon512', 'Falcon512', 'Falcon512'),
     ('kem', 'X25519', 'RSA2048', 'RSA2048', 'RSA2048'),
     ('kem', 'sikep434compressed', 'Falcon512', 'XMSS', 'RainbowIaCyclic'),
+    ('kem', 'sikep434compressed', 'Falcon512', 'XMSS', 'Gemss128'),
     ('kem', 'sikep434compressed', 'Falcon512', 'RainbowIaCyclic', 'RainbowIaCyclic'),
+    ('kem', 'sikep434compressed', 'Falcon512', 'Gemss128', 'Gemss128'),
     ('kem', 'kyber512', 'Dilithium2', 'Dilithium2', 'Dilithium2',),
     ('kem', 'ntruhps2048509', 'Falcon512', 'Falcon512', 'Falcon512'),
 )
 
-LATENCIES = ['2.684ms', '15.458ms', '39.224ms', '97.73ms']
-LOSS_RATES = [0, 0.1] # 0.1, 0.5, 1, 1.5, 2, 2.5, 3] + list(range(4, 21)):
+#LATENCIES = ['2.684ms', '15.458ms', '39.224ms', '97.73ms']
+LATENCIES = ['15.458ms', '97.73ms']
+LOSS_RATES = [0, 5]     # 0.1, 0.5, 1, 1.5, 2, 2.5, 3] + list(range(4, 21)):
 NUM_PINGS = 50  # for measuring the practical latency
 
 
