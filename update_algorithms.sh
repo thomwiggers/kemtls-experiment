310bf747033085324c8d026e52ea5d1235fc9c46
Thom Wiggers <thom@thomwiggers.nl>
2021-02-12 17:29:00 +0100

    Fix client auth in tests

--- update_algorithms.sh
+++ update_algorithms.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+pushd mk-cert/kemutil
+cargo update
+popd
+pushd mk-cert/signutil
+cargo update
+popd
 
 pushd rustls
 python generate_schemes.py
@@ -7,4 +15,15 @@ popd
 pushd webpki/src
 python generate_schemes.py
 git add data generated
+popd
+
+pushd rustls/test-ca
+
+for dir in kyber dilithium; do
+    pushd $dir
+    bash cp_from_mkcert.sh
+    git add .
+    popd
+done
+
 popd
