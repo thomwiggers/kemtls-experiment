fe1ae3252b572d86ba034f1a38d9dab7a4194bef
Thom Wiggers <thom@thomwiggers.nl>
2021-06-07 12:09:45 +0200

    make newer results available

--- measuring/archived-results/download.sh
+++ measuring/archived-results/download.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget 'https://kemtls.s3.amazonaws.com/archived-results/data-2020-05-03.tar.xz'
 wget 'https://kemtls.s3.amazonaws.com/archived-results/data-2020-06-12.tar.xz'
+wget 'https://kemtls.s3.amazonaws.com/archived-results/data-2021-05-07.tar.xz'
